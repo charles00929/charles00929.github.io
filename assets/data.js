34207a89c19f2d973624cefe7d9a1878bfdc47fa
Charles <charles00929@gmail.com>
2020-02-06 22:40:03 +0800

    version 1

---
+++ assets/data.js
@@ -0,0 +1,199 @@
+const portfolio = [
+    {
+        "name": "瑞德利手錶"
+        ,"type": "cart"
+        ,"images": ["readily_01.jpg"]
+        ,"url": "https://www.watch-readily.com.tw/"
+        ,"description": ""
+    }
+    ,{
+        "name": "班尼頓服飾"
+        ,"type": "cart"
+        ,"images": ["benetton_01.jpg"]
+        ,"url": "https://www.benetton.com"
+        ,"description": ""
+    }
+    // ,{
+    //     "name": "大前展威運動用品"
+    //     ,"type": "cart"
+    //     ,"images": ["no_image.jpg"]
+    //     ,"url": "https://www.team-uniform.com.tw"
+    //     ,"description": ""
+    // }
+    ,{
+        "name": "wellswim大前展威-姐妹站"
+        ,"type": "cart"
+        ,"images": ["wellswim_01.jpg"]
+        ,"url": "https://www.wellswim.com.tw"
+        ,"description": ""
+    }
+    ,{
+        "name": "山進電子"
+        ,"type": "cart"
+        ,"images": ["sangean_01.jpg"]
+        ,"url": "https://www.sangean.com.tw/"
+        ,"description": ""
+    }
+    ,{
+        "name": "亞拉機油"
+        ,"type": "cart"
+        ,"images": ["lubrichem_01.jpg"]
+        ,"url": "https://www.lubrichem.com.tw"
+        ,"description": ""
+    }
+    // ,{
+    //     "name": "日藥本舖-2017(終止營運)"
+    //     ,"type": "cart"
+    //     ,"images": ["no_image.jpg"]
+    //     ,"url": "#"
+    //     ,"description": ""
+    // }
+    ,{
+        "name": "泰山汕頭火鍋"
+        ,"type": "cart"
+        ,"images": ["taisanshiu_01.jpg"]
+        ,"url": "https://www.taisanshiu.com.tw/"
+        ,"description": ""
+    }
+    ,{
+        "name": "鷗芭旅遊"
+        ,"type": "cart"
+        ,"images": ["obbatour_01.jpg"]
+        ,"url": "https://www.obbatour.com"
+        ,"description": ""
+    }
+    ,{
+        "name": "歐格副食品"
+        ,"type": "cart"
+        ,"images": ["ogre_01.jpg"]
+        ,"url": "http://www.ogre.com.tw/"
+        ,"description": ""
+    }
+    ,{
+        "name": "有健康醫療"
+        ,"type": "cart"
+        ,"images": ["uhealthshop_01.jpg"]
+        ,"url": "https://www.uhealthshop.com"
+        ,"description": ""
+    }
+    ,{
+        "name": "well原林電子"
+        ,"type": "image"
+        ,"images": ["well_01.jpg"]
+        ,"url": "http://www.well.com.tw/"
+        ,"description": ""
+    }
+    ,{
+        "name": "自在空間"
+        ,"type": "image"
+        ,"images": ["fzhair_01.jpg"]
+        ,"url": "https://www.fzhair.com.tw"
+        ,"description": ""
+    }
+    ,{
+        "name": "關懷弱勢協會"
+        ,"type": "image"
+        ,"images": ["chungwaankang_01.jpg"]
+        ,"url": "https://www.chungwaankang.com"
+        ,"description": ""
+    }
+    //     ,{
+    //     "name": "全美生技 - 終止營運"
+    //     ,"type": "cart"
+    //     ,"images": ["no_image.jpg"]
+    //     ,"url": "#"
+    //     ,"description": ""
+    // }
+    ,{
+        "name": "雄鼎食品行"
+        ,"type": "cart"
+        ,"images": ["sdbestfood_01.jpg"]
+        ,"url": "https://www.sdbestfood.com"
+        ,"description": ""
+    }
+    ,{
+        "name": "法裁西服"
+        ,"type": "cart"
+        ,"images": ["facha_01.jpg"]
+        ,"url": "https://www.facha.com.tw"
+        ,"description": ""
+    }
+    ,{
+        "name": "樂享企業"
+        ,"type": "cart"
+        ,"images": ["happybrick_01.jpg"]
+        ,"url": "https://www.happybrickhappybrick.com/"
+        ,"description": ""
+    }
+    ,{
+        "name": "芮薇荷SPA"
+        ,"type": "cart"
+        ,"images": ["reverforetspa_01.jpg"]
+        ,"url": "https://www.reverforetspa.com"
+        ,"description": ""
+    }
+    ,{
+        "name": "元健聽力"
+        ,"type": "image"
+        ,"images": ["digibionic_01.jpg"]
+        ,"url": "https://www.digibionic.com"
+        ,"description": ""
+    }
+    ,{
+        "name": "優可制服"
+        ,"type": "image"
+        ,"images": ["urcode_01.jpg"]
+        ,"url": "https://www.urcode.com.tw"
+        ,"description": ""
+    }
+    ,{
+        "name": "宸軒法律事務所"
+        ,"type": "image"
+        ,"images": ["yangandwang_01.jpg"]
+        ,"url": "https://www.yangandwang.com/"
+        ,"description": ""
+    }
+    ,{
+        "name": "捷德科技"
+        ,"type": "image"
+        ,"images": ["jedotw_01.jpg"]
+        ,"url": "https://www.jedotw.com/"
+        ,"description": ""
+    }
+    ,{
+        "name": "強淞企業"
+        ,"type": "cart"
+        ,"images": ["select-stainless_01.jpg"]
+        ,"url": "https://www.select-stainless.com"
+        ,"description": ""
+    }
+    ,{
+        "name": "溢哲有限公司"
+        ,"type": "image"
+        ,"images": ["YehJeh_01.jpg"]
+        ,"url": "https://www.yehjeh.com"
+        ,"description": ""
+    }
+    ,{
+        "name": "英凡地毯"
+        ,"type": "image"
+        ,"images": ["innovative_01.jpg"]
+        ,"url": "https://www.innovativeworkplacetw.com"
+        ,"description": ""
+    }
+    ,{
+        "name": "Roxim 祐泰"
+        ,"type": "image"
+        ,"images": ["roxim_01.jpg"]
+        ,"url": "https://stage.roxim.net"
+        ,"description": ""
+    }
+    // ,{
+    //     "name": ""
+    //     ,"type": ""
+    //     ,"images": [""]
+    //     ,"url": ""
+    //     ,"description": ""
+    // }
+
+];
